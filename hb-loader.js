4e29c5e3cbae8258ed198c62854e749fa2805530
Harendra Rathore <harendra@legion.is>
2026-02-04 18:23:05 +0530

    Initial staging setup

---
+++ hb-loader.js
@@ -0,0 +1,6 @@
+(function () {
+  var hbScript = document.createElement("script");
+  hbScript.async = true;
+  hbScript.src = "https://my.hellobar.com/4093d06fd4de0bf422a37110bed1ede31e999683.js";
+  document.head.appendChild(hbScript);
+})();
